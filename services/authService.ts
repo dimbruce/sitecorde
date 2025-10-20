336e8db033aa0ca50aa119b483c2230aec1f2a2d
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-10-20 13:57:50 -0400

    Updated auth service to latest firebase

--- services/authService.ts
+++ services/authService.ts
@@ -1,19 +1,16 @@
-
-import firebase from 'firebase/compat/app';
-import { getAuth } from "./firebaseService";
+import { auth } from "./firebaseService";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 // sign in with Google
 export async function signIn() {
-  const auth = await getAuth();
   // Instantiate provider just-in-time to avoid premature auth initialization
-  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
   // Persistence is now handled in getAuth().
   // Using signInWithPopup to be compatible with iframe environments.
-  await auth.signInWithPopup(googleProvider);
+  await signInWithPopup(auth, googleProvider);
 }
 
 // sign out
 export async function logOut() {
-  const auth = await getAuth();
-  await auth.signOut();
+  await signOut(auth);
 }
